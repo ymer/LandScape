dc67feb1881906e17a68f925c9b63ee3d79ff5c3
jsmp <jonatan.smp@gmail.com>
2016-03-21 10:37:56 +0100

    v 1.0

--- README.md
+++ README.md
@@ -1,21 +1,51 @@
-# LandScape
-A simple method to aggregate p values without a priori grouping
+LandScape - A simple method to aggregate p values without a priori grouping
 
-For a description of the method read LandScape.pdf
-
-For help on how to run the program run the command: 
-python landscape.py --help
+For help on how to run the program run the command: python landscape.py --help
 
 If plink files are used as input, the package https://github.com/ymer/Plink is required.
 
 Output files:
 
-1. An output files containing the maximal segments and their values. This file is saved to [output]_[score_evaluation].txt.  
-It contains the following columns:  
-s - The start point of the maximal segment.  
-e - The end point of the maximal segment.  
-Y - The height of the highest point in the maximal segment.  
-p_value - The p value of the maximal segment.  
+An output files containing the maximal segments and their values. This file is saved to [output]_[score_evaluation].txt.
+It contains the following columns:
+s - The start point of the maximal segment.
+e - The end point of the maximal segment.
+Y - The height of the highest point in the maximal segment.
+p_value - The p value of the maximal segment.
 adjusted_p_value - The p value of the maximal segment adjusted for multiple testing.
 
-2. A file containing the A_k values. This file is saved to [output]_Ak.txt
+A file containing the A_k values. This file is saved to [output]_Ak.txt
+
+Arguments:
+
+  --infile INFILE       
+                        The input file path
+                        
+  --outfile OUTFILE     
+                        The output file path
+                        
+  --input_format {text,gzip,plink}
+                        Default: text
+                        
+  --transformation {dichotomous,log,none}
+                        Transformation of the input data. dichotomous applies the 1/-1 transformation shown example 1, and log applies the log transformation shown in example 2.
+                        Default: log
+                        
+  --score_evaluation {A0,A1,A2}
+                        How to evaluate the scores. (See section 4.2-4.4).
+                        
+  --gamma GAMMA         
+                        The threshold of significance. (See Example 1 and 2).
+                        Default: 0.05
+                        
+  --no-lattice          Set if Z is not a lattice variable. (See definition 8 and 9)
+                        
+  --permutations PERMUTATIONS
+                        The number of permutations that are performed if score_evaluation is A1 or A2.
+                        Default: If input_format is text or gzip, the number of lines in the input file.
+                        
+  --random_seed RANDOM_SEED
+                        Random seed for permutations if input_format is plink.
+                        Default: 1
+                        
+  --no-draw             Do not draw the landscape
--- landscape.py
+++ landscape.py
@@ -1,142 +1,109 @@
 from __future__ import division
-import random
 import math
 import gzip
-from math import log
-from math import pow
+from math import log, pow
 import sys
 import argparse
+from collections import namedtuple
+import pandas as pd
+import plink
+import random
+import numpy as np
+
+Landscape = namedtuple('Landscape', 'A s e Y position_Y sections section_indices maximal_segments')
+MaximalSegment = namedtuple('Maximal', 's e sec_s sec_e Y independent')
 
 
-class Landscape:
-    def __init__(self, A, s, e, Y, position_Y, sections, section_indices, maximal_segments):
-        self.A = A
-        self.s = s
-        self.e = e
-        self.Y = Y
-        self.position_Y = position_Y
-        self.sections = sections
-        self.section_indices = section_indices
-        self.maximal_segments = maximal_segments
-
-
-class MaximalSegment:
-    def __init__(self, s, e, sec_s, sec_e, Y, independent):
-        self.s = s
-        self.e = e
-        self.sec_s = sec_s
-        self.sec_e = sec_e
-        self.Y = Y
-        self.independent = independent
-
-
-def landscape_generator_plink(inds, snps, settings):
-    def shuffle_affection(inds):
-        aff = [ind.aff for ind in inds]
-        random.shuffle(aff)
-        for i in range(len(inds)):
-            inds[i].aff = aff[i]
-        return inds
-
-    random.seed(settings["random_seed"])
+def landscape_generator_plink():
+    inds, snps, genos = plink.read_plink(settings.infile)
+    random.seed(settings.random_seed)
     while 1:
-        inds = shuffle_affection(inds)
-        f.calc_p(inds, snps)
-        yield get_landscape_from_plink(snps, settings)
+        inds.pheno = np.random.permutation(inds.pheno)
+        pvals = plink.assoc(inds, genos)
+        yield get_landscape(pvals)
 
 
-def get_landscape_from_plink(snps, settings):
-    assert settings["transformation"] in ["dichotomous", "log"]
-    if settings["transformation"] == "dichotomous":
-        return get_landscape([1 if snp.p < settings["gamma"] else -1 for snp in snps])
-    elif settings["transformation"] == "log":
-        return get_landscape([log(settings["gamma"] / snp.p) for snp in snps])
-
-
-def landscape_generator_file(settings):
-    if settings["input_type"] == "gzip":
-        with gzip.open(settings["input"] + ".gz") as input:
-            next(input)
-            for line in input:
-                line = line.split()
-                yield get_landscape_from_file(settings, line)
-    else:
-        with open(settings["input"] + ".txt") as input:
-            next(input)
-            for line in input:
-                line = line.split()
-                yield get_landscape_from_file(settings, line)
-
-
-def get_landscape_from_file(settings, line):
-    if settings["transformation"] == "dichotomous":
-        return get_landscape([1 if float(value) < settings["gamma"] else -1 for value in line])
-    elif settings["transformation"] == "log":
-        return get_landscape([log(settings["gamma"] / float(elem)) for elem in line])
-    elif settings["transformation"] == "none":
-        return get_landscape([float(elem) for elem in line])
+def landscape_generator_file():
+    inp = open(settings.infile) if not settings.input_format == 'gzip' else gzip.open(settings.infile)
+    next(inp)
+    for line in inp:
+        yield get_landscape([float(value) for value in line.split()])
+    inp.close()
 
 
 def write_results(landscape, pvalues, ntests, settings):
 
-    titles = ["s", "e", "Y", "type", "p_value", "adjusted_p_value"]
+    columns = ['s', 'i', 'Y', 'type', 'p_value', 'adjusted_p_value']
 
-    values = [[str(segment.s), str(segment.e), str(segment.Y), "independent" if segment.independent else "dependent",
-              str(round(min(pvalues[k], 1), 6)), str(min(pvalues[k] * ntests, 1))]
+    values = [[str(segment.s),
+               str(segment.e),
+               str(segment.Y),
+               'independent' if segment.independent else 'dependent',
+               str(round(min(pvalues[k], 1), 6)),
+               str(min(pvalues[k] * ntests, 1))]
               for k, segment in enumerate(landscape.maximal_segments)]
 
-    f.write(settings["output"] + "_" + settings["score_evaluation"] + ".txt", values, titles, settings["format_columns"])
-
-
-def write_ak_plink(snps, snp_p, landscape, settings):
-        titles = ["chr", "bp", "p_value", "A_k"]
-        values = [[snps[i].chr, snps[i].pos, snp_p[i], landscape.A[i]] for i in range(len(snps))]
-        f.write(settings["output"] + "_A_k.txt", values, titles, settings["format_columns"])
+    df = pd.DataFrame(values, columns=columns)
+    path = settings.outfile + '_' + settings.score_evaluation + '.txt'
+    df.to_csv(path, index=False, sep='\t')
 
 
 def write_ak_text(snp_p, landscape, settings):
-        titles = ["position", "p_value", "A_k"]
-        values = [[str(i), snp_p[i], landscape.A[i]] for i in range(len(snp_p))]
-        f.write(settings["output"] + "_A_k.txt", values, titles, settings["format_columns"])
+    columns = ['position', 'p_value', 'A_k']
+    values = [[str(i),
+               snp_p[i],
+               landscape.A[i]]
+              for i in range(len(snp_p))]
+
+    df = pd.DataFrame(values, columns=columns)
+    path = settings.outfile + '_A_k.txt'
+    df.to_csv(path, index=False, sep='\t')
 
 
-def get_pvalues_approach1(landscape, landscape_generator, settings):
+def get_pvalues_A0(landscape):
+    if settings.lattice:
+        lmbda = math.log((1 - settings.gamma) / settings.gamma)
+        return [(1 - pow(math.e, -lmbda))*pow(math.e, -lmbda*segment.Y) for segment in landscape.maximal_segments]
+    else:
+        from scipy.optimize import minimize_scalar
+        lmbda = minimize_scalar(lambda x: abs(log(1 - x) / x - log(settings.gamma)),
+                                bounds=(0.001, 0.9999), method='bounded').x
+        return [pow(math.e, -lmbda*segment.Y) for segment in landscape.maximal_segments]
 
-    def get_hits_per_segment(landscape, y):
-        hits_in_permutation = sum(1 for segment in landscape.maximal_segments if segment.Y >= y)
-        if landscape.maximal_segments:
-            return hits_in_permutation / len(landscape.maximal_segments)
-        else:
-            return 0
 
-    M = [len(landscape.maximal_segments)]
-    for segment in landscape.maximal_segments:
-        segment.hits = [get_hits_per_segment(landscape, segment.Y)]
+def get_pvalues_A1(orig_landscape, landscape_generator):
 
-    for _ in range(settings["permutations"]):
-        new_landscape = landscape_generator.next()
+    def add_hits(landscape):
+        for orig_segment in orig_landscape.maximal_segments:
+            hits_in_permutation = sum(1 for segment in landscape.maximal_segments if segment.Y >= orig_segment.Y)
+            hits_per_segment = hits_in_permutation / len(landscape.maximal_segments) if landscape.maximal_segments else 0
+            hits[orig_segment.s].append(hits_per_segment)
+
+    hits = {segment.s: [0] for segment in orig_landscape.maximal_segments}
+
+    M = [len(orig_landscape.maximal_segments)]
+    add_hits(orig_landscape)
+
+    for _ in range(settings.permutations):
+        new_landscape = next(landscape_generator)
         M.append(len(new_landscape.maximal_segments))
-        for segment in landscape.maximal_segments:
-            segment.hits.append(get_hits_per_segment(new_landscape, segment.Y))
+        add_hits(new_landscape)
 
-    pvalues = []
-    for segment in landscape.maximal_segments:
-        pvalues.append(sum(segment.hits) / (settings["permutations"] + 1))
-
+    pvalues = [sum(hits[segment.s]) / (settings.permutations + 1) for i, segment in enumerate(orig_landscape.maximal_segments)]
     EM = sum(M) / len(M)
 
     return pvalues, EM
 
 
-def get_pvalues_approach2(landscape, landscape_generator, settings):
+def get_pvalues_A2(landscape, landscape_generator):
 
     tops = [k for k in range(len(landscape.position_Y)) if landscape.position_Y[k]]
     hits = [0 for _ in range(len(landscape.A))]
     perms = [0 for _ in range(len(landscape.A))]
     M = [len(landscape.maximal_segments)]
 
-    for _ in range(settings["permutations"]):
-        new_landscape = landscape_generator.next()
+    for _ in range(settings.permutations):
+        new_landscape = next(landscape_generator)
         if new_landscape.maximal_segments:
             M.append(len(new_landscape.maximal_segments))
             for k in tops:
@@ -145,41 +112,23 @@ def get_pvalues_approach2(landscape, landscape_generator, settings):
                     if landscape.position_Y[k] <= new_landscape.position_Y[k]:
                         hits[k] += 1
     if any(perms[i] < 1 for segment in landscape.maximal_segments for i in range(segment.s, segment.e + 1)):
-        print "More permutations are required for approach 2"
+        print('More permutations are required for approach 2')
         assert not any(perms[i] < 1 for i in range(len(landscape.A)))
 
-    pvalues = []
-    for segment in landscape.maximal_segments:
-        pvalues_segment = [hits[i] / perms[i] for i in range(segment.s, segment.e + 1)]
-        pvalues.append(max(pvalues_segment))
-
+    pvalues = [max([hits[i] / perms[i] for i in range(segment.s, segment.e + 1)]) for segment in landscape.maximal_segments]
     EM = sum(M) / len(M)
 
     return pvalues, EM
 
 
-def get_pvalues_independent(landscape, settings):
-    pvalues = []
-    if settings["lattice"]:
-        lmbda = math.log((1 - settings["gamma"]) / settings["gamma"])
-        for segment in landscape.maximal_segments:
-            pvalue = (1 - pow(math.e, - lmbda)) * pow(math.e, -lmbda * (segment.Y - settings["minimum_score"]))
-            pvalues.append(pvalue)
+def get_landscape(values):
+    if settings.transformation == 'dichotomous':
+        Z = [1 if value < settings.gamma else -1 for value in values]
+    elif settings.transformation == 'log':
+        Z = [log(settings.gamma / value) for value in values]
     else:
-        from scipy.optimize import minimize_scalar
+        Z = values
 
-        def f(x):
-            return abs(log(1 - x) / x - log(settings["gamma"]))
-
-        lmbda = minimize_scalar(f, bounds=(0.001, 0.9999), method='bounded').x
-        for segment in landscape.maximal_segments:
-            pvalue = lmbda * pow(math.e, -lmbda * (segment.Y - settings["minimum_score"]))
-            pvalues.append(pvalue)
-
-    return pvalues
-
-
-def get_landscape(Z):
     A = [0]
     for n in range(len(Z)):
         A.append(max(0, A[n] + Z[n]))
@@ -218,70 +167,83 @@ def get_landscape(Z):
     maximal_segments = []
     for i in section_indices:
         for j in range(len(s[i])):
-                maximal_segments.append(MaximalSegment(s[i][j], e[i][j], sections[i][0], sections[i][-1], Y[i][j], j == 0))
-                for k in range(s[i][j], e[i][j] + 1):
-                    position_Y[k] = Y[i][j]
+            maximal_segments.append(MaximalSegment(s[i][j], e[i][j], sections[i][0], sections[i][-1], Y[i][j], j == 0))
+            for k in range(s[i][j], e[i][j] + 1):
+                position_Y[k] = Y[i][j]
 
     return Landscape(A, s, e, Y, position_Y, sections, section_indices, maximal_segments)
 
 
-def run(settings):
-    if settings["input_type"] == "plink":
-        inds, snps = f.read_plink(settings["input"])
-        f.calc_p(inds, snps)
-        orig_snp_p = [snp.p for snp in snps]
-        landscape = get_landscape_from_plink(snps, settings)
-        landscape_generator = landscape_generator_plink(inds, snps, settings)
+def run():
+
+    if settings.input_format != 'plink':
+        inp = open(settings.infile) if not settings.input_format == 'gzip' else gzip.open(settings.infile)
+        line = inp.readline()
+        orig_values = [float(value) for value in line.split()]
+        if not settings.permutations:
+            settings.permutations = sum(1 for _ in inp)
+        inp.close()
+
+        landscape = get_landscape(orig_values)
+        landscape_generator = landscape_generator_file()
     else:
-        if settings["input_type"] == "gzip":
-            with gzip.open(settings["input"] + ".gz") as input:
-                line = input.readline()
-                orig_snp_p = line.split()
-        elif settings["input_type"] == "text":
-            with open(settings["input"] + ".txt") as input:
-                line = input.readline()
-                orig_snp_p = line.split()
-        landscape = get_landscape_from_file(settings, orig_snp_p)
-        landscape_generator = landscape_generator_file(settings)
+        inds, snps, genos = plink.read_plink(settings.infile)
+        orig_values = plink.assoc(inds, genos)
+        landscape = get_landscape(orig_values)
+        landscape_generator = landscape_generator_plink()
 
-    if settings["permutations"] > 0:
-        if settings["score_evaluation"] == "approach1":
-            pvalues, EM = get_pvalues_approach1(landscape, landscape_generator, settings)
-
-        if settings["score_evaluation"] == "approach2":
-            pvalues, EM = get_pvalues_approach2(landscape, landscape_generator, settings)
-
-        if settings["score_evaluation"] == "independent":
-            pvalues = get_pvalues_independent(landscape, settings)
+    if settings.permutations > 0:
+        if settings.score_evaluation == 'A1':
+            pvalues, EM = get_pvalues_A1(landscape, landscape_generator)
+        elif settings.score_evaluation == 'A2':
+            pvalues, EM = get_pvalues_A2(landscape, landscape_generator)
+        else:
+            pvalues = get_pvalues_A0(landscape)
             EM = len(pvalues)
     else:
         pvalues = [1 for _ in landscape.maximal_segments]
         EM = 1
 
     write_results(landscape, pvalues, EM, settings)
-
-    if settings["input_type"] == "plink":
-        write_ak_plink(snps, orig_snp_p, landscape, settings)
-    else:
-        write_ak_text(orig_snp_p, landscape, settings)
+    write_ak_text(orig_values, landscape, settings)
 
 
 def get_settings(args):
-    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter, prefix_chars='--')
-    parser.add_argument('--infile', help="The input file path")
-    parser.add_argument('--outfile', help="The output file path")
-    parser.add_argument('--minimum_score', help="The minimum score if independent is chosen for multiple_testing_adjustment. (y_0 in formula 3.8 / 3.9)")
-    parser.add_argument('--score_evaluation', help="How to evaluate the scores. Approaches explained in section 3.1 and section 3.2.")
-    parser.add_argument('--permutations', help="The number of permutations that are performed if approach1 or approach2 is chosen for multiple_testing_adjustment. (Note, if input type 'text' is used, the program still expects the number of permutations to be set. It can be set to the full number of permutations in the file, or to a lower number, but not to a higher number.)")
-    parser.add_argument('--gamma', help="The threshold of significance. (See Example 3.1 and 3.2).")
-    parser.add_argument('--lattice', help="Set to true if Z is a lattice variable. (See example 3.1)")
-    parser.add_argument('--random_seed', default=1)
+    parser = argparse.ArgumentParser(prefix_chars='--', formatter_class=argparse.RawTextHelpFormatter, add_help=False)
+    parser.add_argument('--help', action='help', help='\nShow this help message and exit\n\n')
+    parser.add_argument('--infile', help='\nThe input file path\n\n', required=True)
+    parser.add_argument('--outfile', help='\nThe output file path\n\n', required=True)
+    parser.add_argument('--input_format', choices=['text', 'gzip', 'plink'], default='text', help='Default: %(default)s\n\n')
+    parser.add_argument('--transformation', choices=['dichotomous', 'log', 'none'], default='log', help='Transformation of the input data. dichotomous applies the 1/-1 transformation shown example 1, and log applies the log transformation shown in example 2.\nDefault: %(default)s\n\n')
+    parser.add_argument('--score_evaluation', help='How to evaluate the scores. (See section 4.2-4.4).\n\n', choices=['A0', 'A1', 'A2'], required=True)
+    parser.add_argument('--gamma', type=float, default=0.05, help='\nThe threshold of significance. (See Example 1 and 2).\nDefault: %(default)s\n\n')
+    parser.add_argument('--no-lattice', dest='lattice', action='store_false', default=True, help= 'Set if Z is not a lattice variable. (See definition 8 and 9)\n\n')
+    parser.add_argument('--permutations', type=int, help='The number of permutations that are performed if score_evaluation is A1 or A2.\nDefault: If input_format is text or gzip, the number of lines in the input file.\n\n')
+    parser.add_argument('--random_seed', default=1, help='Random seed for permutations if input_format is plink.\nDefault: %(default)s\n\n')
+    parser.add_argument('--no-draw', dest='draw', action='store_false', help='Do not draw the landscape')
 
     return parser.parse_args(args)
+
+
+def draw():
+    import matplotlib.pyplot as plt
+    ak = pd.read_csv(settings.outfile + '_A_k.txt', sep='\s+')
+    ak['logp'] = ak.p_value.apply(lambda x: -math.log10(x))
+    plt.plot(ak.position, ak.logp)
+    sig = -math.log10(0.05 / len(ak))
+    plt.plot([min(ak.position), max(ak.position)], [sig, sig], color='0.75', linestyle='--')
+    plt.ylabel('-log10(p-value)')
+    plt.xlabel('position')
+
+    fig = plt.gcf()
+    fig.set_size_inches(18.5, 10.5)
+    fig.savefig(settings.outfile + '.png', dpi=100)
 
 
 if __name__ == "__main__":
 
     settings = get_settings(sys.argv[1:])
+    run()
 
-    run(settings)
+    if settings.draw:
+        draw()
